4ac1f3f111677ca6d0d183b9d62a1b02748c710e
Paul Sealock <psealock@gmail.com>
2018-04-18 14:06:14 -1000

    empty ref-by-ref state

--- client/state/selectors/get-store-referrers-by-referrer.js
+++ client/state/selectors/get-store-referrers-by-referrer.js
@@ -13,12 +13,11 @@ import { getSiteStatsNormalizedData } from 'state/stats/lists/selectors';
 
 export default function( state, { siteId, statType, query, selectedReferrer } ) {
 	const rawData = getSiteStatsNormalizedData( state, siteId, statType, query );
-	if ( ! selectedReferrer ) {
-		return [];
-	}
 	return rawData.map( d => {
 		const { data, ...props } = d;
-		const referrerData = find( data, r => r.referrer === selectedReferrer ) || {};
+		const referrerData = selectedReferrer
+			? find( data, r => r.referrer === selectedReferrer ) || {}
+			: {};
 		return {
 			...props,
 			...referrerData,
