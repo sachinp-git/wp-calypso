41f2c0f6f5f1d70a211937e19a7b131366b927ae
Tugdual de Kerviler <dekervit@gmail.com>
2018-04-19 13:57:24 +0200

    Update format for location and make location card use real data in stats

--- client/my-sites/google-my-business/stats/fake-data.js
+++ client/my-sites/google-my-business/stats/fake-data.js
@@ -64,20 +64,6 @@ const statFunction = ( statType, period ) => {
 	);
 };
 
-const locationData = {
-	id: 12345,
-	address: [
-		'Centre Commercial Cap 3000',
-		'Avenue Eugene Donadei',
-		'06700 Saint-Laurent-du-Var',
-		'France',
-	],
-	name: 'Starbucks',
-	photo: 'http://www.shantee.net/wp-content/uploads/2016/02/cookies-internet-1030x684.jpg',
-	verified: true,
-};
-
 export default {
 	statFunction,
-	locationData,
 };
