8e9f4f06067d8f67bf2d0a0e864d5403566b3e99
Adam Zieliński <adam@adamziel.com>
2018-04-24 09:52:56 +0200

    Change two year to biennial

--- client/my-sites/checkout/cart/cart-item.jsx
+++ client/my-sites/checkout/cart/cart-item.jsx
@@ -207,7 +207,7 @@ export class CartItem extends React.Component {
 		} else if ( isYearly( cartItem ) ) {
 			return translate( 'annual subscription' );
 		} else if ( isBiennially( cartItem ) ) {
-			return translate( 'biennial subscription' );
+			return translate( 'two year subscription' );
 		}
 
 		return false;
